aa1707638ee690df7b4b6a79b3c0c6c5853ca61e
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-10-27 21:32:37 +0330

    test22

--- main.js
+++ main.js
@@ -10,6 +10,31 @@ const text = document.getElementById("box1");
 const button = document.getElementById("button");
 
 button.addEventListener("click", () => {
-  text.style.backgroundColor = "red";
+  text.style.backgroundColor = "green";
   text.style.transition = "2s";
 });
+
+// Test-2
+
+let kilik = document.getElementById("bezan");
+let matn = document.getElementById("texty");
+
+kilik.addEventListener("click", () => {
+  matn.style.color = "#ffffffc9";
+  matn.style.transition = "2s";
+  matn.style.textAlign = "center";
+});
+kilik.addEventListener("click", () => {
+  matn.style.fontSize = "19px";
+  console.log(matn);
+});
+console.log(typeof matn);
+
+// s
+
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Test Man";
+document.body.appendChild(newParagraph);
+newParagraph.style.backgroundColor = "Violet";
+newParagraph.style.color = "white";
+newParagraph.style.fontSize = "45px";
